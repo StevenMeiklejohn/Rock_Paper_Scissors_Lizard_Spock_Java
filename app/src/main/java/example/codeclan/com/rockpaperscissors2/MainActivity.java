6c5890e40c1bdcf1e286c1ec1ad720993d8a947e
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-03-02 10:55:30 +0000

    Rock, paper, scissors, lizard, spock fully implemented and working

--- app/src/main/java/example/codeclan/com/rockpaperscissors2/MainActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors2/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageButton b_rock, b_paper, b_scissors;
+    ImageButton b_rock, b_paper, b_scissors, b_lizard, b_spock;
     ImageView rules;
     TextView scoreText1, scoreText2, tv_score;
     ImageView iv_computer;
@@ -36,15 +36,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        initialise button(s)
         rules = (ImageView) findViewById(R.id.rules);
         b_rock = (ImageButton) findViewById(R.id.b_rock);
-
-
         b_paper = (ImageButton) findViewById(R.id.b_paper);
-        b_paper.setColorFilter(Color.argb(255, 255, 255, 255));
-
+//        b_paper.setColorFilter(Color.argb(255, 255, 255, 255));
         b_scissors = (ImageButton) findViewById(R.id.b_scissors);
-        b_paper.setColorFilter(Color.argb(255, 255, 255, 255));
-
         tv_score = (TextView) findViewById(R.id.tv_score);
+        b_lizard = (ImageButton) findViewById(R.id.b_lizard);
+        b_spock = (ImageButton) findViewById(R.id.b_spock);
 
 
 //        MainActivity.this is a Current Class Reference (context)
@@ -52,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         b_rock.setOnClickListener(MainActivity.this);
         b_paper.setOnClickListener(MainActivity.this);
         b_scissors.setOnClickListener(MainActivity.this);
+        b_lizard.setOnClickListener(MainActivity.this);
+        b_spock.setOnClickListener(MainActivity.this);
         tv_score.setOnClickListener(MainActivity.this);
     }
 
@@ -88,6 +87,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 tv_score.setText("Score Human: " + Integer.toString(humanScore) + " Computer: " + Integer.toString(computerScore));
                 Log.d("RockPaperScissors", "b_scissors clicked");
                 break;
+            case R.id.b_lizard:
+//                b_scissors.getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFAA0000));
+                ImageButton b_lizard = (ImageButton)findViewById(R.id.b_lizard);
+                b_lizard.setBackgroundResource(R.drawable.abc_lizard);
+                String message4 = playTurn("lizard");
+                Toast.makeText(MainActivity.this, message4, Toast.LENGTH_SHORT).show();
+                tv_score.setText("Score Human: " + Integer.toString(humanScore) + " Computer: " + Integer.toString(computerScore));
+                Log.d("RockPaperScissors", "b_lizard clicked");
+                break;
+            case R.id.b_spock:
+//                b_scissors.getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFAA0000));
+                ImageButton b_spock = (ImageButton)findViewById(R.id.b_spock);
+                b_spock.setBackgroundResource(R.drawable.abc_spock);
+                String message5 = playTurn("spock");
+                Toast.makeText(MainActivity.this, message5, Toast.LENGTH_SHORT).show();
+                tv_score.setText("Score Human: " + Integer.toString(humanScore) + " Computer: " + Integer.toString(computerScore));
+                Log.d("RockPaperScissors", "b_spock clicked");
+                break;
         }
     }
 
@@ -101,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public String playTurn(String player_choice) {
         String computer_choice = "";
         Random r = new Random();
-        int computer_choice_number = r.nextInt(3) + 1;
+        int computer_choice_number = r.nextInt(5) + 1;
 
         //Determine computer choice.
         if (computer_choice_number == 1) {
@@ -112,6 +129,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if (computer_choice_number == 3) {
             computer_choice = "scissors";
+        }
+        if (computer_choice_number == 4) {
+            computer_choice = "lizard";
+        }
+        if (computer_choice_number == 5) {
+            computer_choice = "spock";
         }
 
         //Set computer choice image
@@ -130,6 +153,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             image.setImageResource(R.drawable.scissors_new);
         }
 
+        if (computer_choice == "lizard") {
+            ImageView image = (ImageView) findViewById(R.id.iv_computer);
+            image.setImageResource(R.drawable.lizard_new);
+        }
+
+        if (computer_choice == "spock") {
+            ImageView image = (ImageView) findViewById(R.id.iv_computer);
+            image.setImageResource(R.drawable.spock_new);
+        }
+
         //Check who wins
         if (computer_choice == player_choice) {
             return "Draw. Nobody wins.";
@@ -142,6 +175,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             computerScore ++;
             return "Paper covers rock. You lose!";
         }
+        else if (player_choice =="rock" && computer_choice == "lizard" ) {
+            humanScore ++;
+            return "Rock smashes lizard. You win!";
+        }
+        else if (player_choice =="rock" && computer_choice == "spock" ) {
+            computerScore ++;
+            return "Spock vaporises rock. You lose!";
+        }
+
+
+
         else if (player_choice =="scissors" && computer_choice == "rock" ) {
             computerScore ++;
             return "Rock blunts scissors. You Lose!";
@@ -150,6 +194,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             humanScore ++;
             return "Scissors cut paper. You win!";
         }
+        else if (player_choice =="scissors" && computer_choice == "lizard" ) {
+            humanScore ++;
+            return "Scissors decapitate lizard. You win!";
+        }
+
+        else if (player_choice =="scissors" && computer_choice == "spock" ) {
+            humanScore ++;
+            return "Spock smashes scissors. You lose!";
+        }
+
+
         else if (player_choice =="paper" && computer_choice == "rock" ) {
             humanScore ++;
             return "Paper covers rocks. You win!";
@@ -157,6 +212,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if (player_choice =="paper" && computer_choice == "scissors" ) {
             computerScore ++;
             return "scissors cuts paper. You lose!";
+        }
+        else if (player_choice =="paper" && computer_choice == "lizard" ) {
+            computerScore ++;
+            return "lizard eats paper. You lose!";
+        }
+        else if (player_choice =="paper" && computer_choice == "spock" ) {
+            computerScore ++;
+            return "Paper disproves Spock. You win!";
+        }
+
+
+        else if (player_choice =="lizard" && computer_choice == "rock" ) {
+            humanScore ++;
+            return "Rock smashes lizard. You lose!!";
+        }
+        else if (player_choice =="lizard" && computer_choice == "scissors" ) {
+            computerScore ++;
+            return "scissors decapitates lizard. You lose!";
+        }
+        else if (player_choice =="lizard" && computer_choice == "paper" ) {
+            computerScore ++;
+            return "lizard eats paper. You win!";
+        }
+        else if (player_choice =="lizard" && computer_choice == "spock" ) {
+            computerScore ++;
+            return "Lizard poisons Spock. You win!";
+        }
+
+
+        else if (player_choice =="spock" && computer_choice == "rock" ) {
+            humanScore ++;
+            return "Spock vaporises rock. You win!";
+        }
+        else if (player_choice =="spock" && computer_choice == "scissors" ) {
+            computerScore ++;
+            return "spock smashes scissors. You win!";
+        }
+        else if (player_choice =="spock" && computer_choice == "lizard" ) {
+            computerScore ++;
+            return "lizard poisons spock. You lose!";
+        }
+        else if (player_choice =="spock" && computer_choice == "paper" ) {
+            computerScore ++;
+            return "Paper disproves Spock. You lose!";
         }
         else return "Damned if I know.";
     }
